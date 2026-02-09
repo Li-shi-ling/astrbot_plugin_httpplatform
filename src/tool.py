47d88f7aa64dea71e54fa70f48a5624c1c08f64a
Lishiling <m18384519631@163.com>
2026-02-09 21:37:46 +0800

    fix:BMC兼容文本图片和音频

--- src/tool.py
+++ src/tool.py
@@ -1,8 +1,10 @@
-from astrbot.api.message_components import Plain, BaseMessageComponent, Record
+from astrbot.api.message_components import Plain, BaseMessageComponent, Record, Image
 
 def BMC2Text(data: BaseMessageComponent):
     if isinstance(data, Plain):
         return data.text, "text"
     if isinstance(data, Record):
         return data.convert_to_base64(), "record"
+    if isinstance(data, Image):
+        return data.convert_to_base64(), "image"
     return data
