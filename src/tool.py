da3b5c4787a9cbd296e52fa9e76519d14389cd11
Lishiling <m18384519631@163.com>
2026-02-15 17:10:03 +0800

    fix:修改astrbot的原生兼容性

--- src/tool.py
+++ src/tool.py
@@ -1,23 +1,5 @@
-from astrbot.api.message_components import (
-    Plain,
-    BaseMessageComponent,
-    Record,
-    Image
-)
+from astrbot.api.message_components import BaseMessageComponent
 import json
 
 def BMC2Text(data: BaseMessageComponent):
-    if isinstance(data, Plain):
-        return data.text, "text"
-    elif isinstance(data, Record):
-        if data.url is None:
-            return data.convert_to_base64(), "record"
-        else:
-            return data.url, "record_url"
-    elif isinstance(data, Image):
-        if data.url is None:
-            return data.convert_to_base64(), "image"
-        else:
-            return data.url, "image_url"
-    else:
-        return json.dumps(data.toDict()), str(data.type)
+    return json.dumps(data.toDict()), str(data.type)
